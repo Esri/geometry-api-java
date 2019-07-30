7f36e3745f215e189e6c6a0b8fb2d64cfc1af31d
Sergey Tolstov <stolstov@esri.com>
2019-07-30 12:17:24 -0700

    formatting in the unit test

--- src/test/java/com/esri/core/geometry/TestOGCCentroid.java
+++ src/test/java/com/esri/core/geometry/TestOGCCentroid.java
@@ -28,63 +28,56 @@ import com.esri.core.geometry.ogc.OGCPoint;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class TestOGCCentroid
-{
-    @Test
-    public void testPoint()
-    {
-        assertCentroid("POINT (1 2)", new Point(1, 2));
-        assertEmptyCentroid("POINT EMPTY");
-    }
+public class TestOGCCentroid {
+	@Test
+	public void testPoint() {
+		assertCentroid("POINT (1 2)", new Point(1, 2));
+		assertEmptyCentroid("POINT EMPTY");
+	}
 
-    @Test
-    public void testLineString()
-    {
-        assertCentroid("LINESTRING (1 1, 2 2, 3 3)", new Point(2, 2));
-        assertEmptyCentroid("LINESTRING EMPTY");
-    }
+	@Test
+	public void testLineString() {
+		assertCentroid("LINESTRING (1 1, 2 2, 3 3)", new Point(2, 2));
+		assertEmptyCentroid("LINESTRING EMPTY");
+	}
 
-    @Test
-    public void testPolygon()
-    {
-        assertCentroid("POLYGON ((1 1, 1 4, 4 4, 4 1))'", new Point(2.5, 2.5));
-        assertCentroid("POLYGON ((1 1, 5 1, 3 4))", new Point(3, 2));
-        assertCentroid("POLYGON ((0 0, 0 5, 5 5, 5 0, 0 0), (1 1, 1 2, 2 2, 2 1, 1 1))", new Point(2.5416666666666665, 2.5416666666666665));
-        assertEmptyCentroid("POLYGON EMPTY");
-    }
+	@Test
+	public void testPolygon() {
+		assertCentroid("POLYGON ((1 1, 1 4, 4 4, 4 1))'", new Point(2.5, 2.5));
+		assertCentroid("POLYGON ((1 1, 5 1, 3 4))", new Point(3, 2));
+		assertCentroid("POLYGON ((0 0, 0 5, 5 5, 5 0, 0 0), (1 1, 1 2, 2 2, 2 1, 1 1))",
+				new Point(2.5416666666666665, 2.5416666666666665));
+		assertEmptyCentroid("POLYGON EMPTY");
+	}
 
-    @Test
-    public void testMultiPoint()
-    {
-        assertCentroid("MULTIPOINT (1 2, 2 4, 3 6, 4 8)", new Point(2.5, 5));
-        assertEmptyCentroid("MULTIPOINT EMPTY");
-    }
+	@Test
+	public void testMultiPoint() {
+		assertCentroid("MULTIPOINT (1 2, 2 4, 3 6, 4 8)", new Point(2.5, 5));
+		assertEmptyCentroid("MULTIPOINT EMPTY");
+	}
 
-    @Test
-    public void testMultiLineString()
-    {
-        assertCentroid("MULTILINESTRING ((1 1, 5 1), (2 4, 4 4))')))", new Point(3, 2));
-        assertEmptyCentroid("MULTILINESTRING EMPTY");
-    }
+	@Test
+	public void testMultiLineString() {
+		assertCentroid("MULTILINESTRING ((1 1, 5 1), (2 4, 4 4))')))", new Point(3, 2));
+		assertEmptyCentroid("MULTILINESTRING EMPTY");
+	}
 
-    @Test
-    public void testMultiPolygon()
-    {
-        assertCentroid("MULTIPOLYGON (((1 1, 1 3, 3 3, 3 1)), ((2 4, 2 6, 6 6, 6 4)))", new Point (3.3333333333333335,4));
-        assertEmptyCentroid("MULTIPOLYGON EMPTY");
-    }
+	@Test
+	public void testMultiPolygon() {
+		assertCentroid("MULTIPOLYGON (((1 1, 1 3, 3 3, 3 1)), ((2 4, 2 6, 6 6, 6 4)))",
+				new Point(3.3333333333333335, 4));
+		assertEmptyCentroid("MULTIPOLYGON EMPTY");
+	}
 
-    private static void assertCentroid(String wkt, Point expectedCentroid)
-    {
-        OGCGeometry geometry = OGCGeometry.fromText(wkt);
-        OGCGeometry centroid = geometry.centroid();
-        Assert.assertEquals(centroid, new OGCPoint(expectedCentroid, geometry.getEsriSpatialReference()));
-    }
+	private static void assertCentroid(String wkt, Point expectedCentroid) {
+		OGCGeometry geometry = OGCGeometry.fromText(wkt);
+		OGCGeometry centroid = geometry.centroid();
+		Assert.assertEquals(centroid, new OGCPoint(expectedCentroid, geometry.getEsriSpatialReference()));
+	}
 
-    private static void assertEmptyCentroid(String wkt)
-    {
-        OGCGeometry geometry = OGCGeometry.fromText(wkt);
-        OGCGeometry centroid = geometry.centroid();
-        Assert.assertEquals(centroid, new OGCPoint(new Point(), geometry.getEsriSpatialReference()));
-    }
+	private static void assertEmptyCentroid(String wkt) {
+		OGCGeometry geometry = OGCGeometry.fromText(wkt);
+		OGCGeometry centroid = geometry.centroid();
+		Assert.assertEquals(centroid, new OGCPoint(new Point(), geometry.getEsriSpatialReference()));
+	}
 }
