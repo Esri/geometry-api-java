c9edc77df866942db7cf1b47fa644b6000bc56c3
Sergey Tolstov <stolstov@esri.com>
2019-11-13 09:33:20 -0800

    Adjust tolerance to include more sig digits, update tests.

--- src/test/java/com/esri/core/geometry/TestWkid.java
+++ src/test/java/com/esri/core/geometry/TestWkid.java
@@ -40,7 +40,7 @@ public class TestWkid extends TestCase {
 
 		SpatialReference sr84 = SpatialReference.create(4326);
 		double tol84 = sr84.getTolerance();
-		assertTrue(Math.abs(tol84 - 1e-8) < 1e-8 * 1e-8);
+		assertTrue(Math.abs(tol84 - 8.9831528411952150e-09) < 1e-15);
 	}
 
 
