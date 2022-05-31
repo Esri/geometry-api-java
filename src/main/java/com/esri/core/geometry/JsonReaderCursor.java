83be9f61e0558eede8e7d420fe7689d212438811
Sergey Tolstov <stolstov@esri.com>
2022-05-31 09:17:48 -0700

    Remove a comment

--- src/main/java/com/esri/core/geometry/JsonReaderCursor.java
+++ src/main/java/com/esri/core/geometry/JsonReaderCursor.java
@@ -21,21 +21,6 @@
 
  email: contracts@esri.com
  */
-/*
- COPYRIGHT 1995-2017 ESRI
-
- TRADE SECRETS: ESRI PROPRIETARY AND CONFIDENTIAL
- Unpublished material - all rights reserved under the
- Copyright Laws of the United States.
-
- For additional information, contact:
- Environmental Systems Research Institute, Inc.
- Attn: Contracts Dept
- 380 New York Street
- Redlands, California, USA 92373
-
- email: contracts@esri.com
- */
 
 package com.esri.core.geometry;
 
