3cfc2683769a12b38195dd341c95d4fd21f3be65
--replace-all <muchembled.martin@gmail.com>
2024-02-20 17:52:45 +0100

    Feat : Created class to get results after tests execution #3

---
+++ src/test/java/com/esri/core/geometry/ResultPrinter.java
@@ -0,0 +1,12 @@
+package com.esri.core.geometry;
+
+import org.junit.Test;
+
+public class ResultPrinter{
+
+    public static void main(String[] args) {
+        BranchCover.resultFileFromName("getCenter");
+        BranchCover.resultFileFromName("importFromESRI");
+    }
+
+}
