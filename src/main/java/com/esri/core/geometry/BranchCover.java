3a4d8a8150574c0451901ffa2c70a041aea68a17
--replace-all <muchembled.martin@gmail.com>
2024-02-20 11:49:18 +0100

    Feat: Created multiple static instances of BranchCover #2

--- src/main/java/com/esri/core/geometry/BranchCover.java
+++ src/main/java/com/esri/core/geometry/BranchCover.java
@@ -5,15 +5,45 @@ public class BranchCover {
     
     private Boolean[] ica;
     private int length;
-    private static BranchCover instance;
+    private static BranchCover instance1;
+    private static BranchCover instance2;
+    private static BranchCover instance3;
+    private static BranchCover instance4;
+    private static BranchCover instance5;
 
-    public static BranchCover getInstance() {
-        if (instance == null) {
-                if (instance == null) {
-                    instance = new BranchCover();
-            }
+    public static BranchCover getInstance1() {        
+        if (instance1 == null) {
+            instance1 = new BranchCover();
         }
-        return instance;
+        return instance1;
+    }
+
+    public static BranchCover getInstance2() {        
+        if (instance2 == null) {
+            instance2 = new BranchCover();
+        }
+        return instance2;
+    }
+
+    public static BranchCover getInstance3() {        
+        if (instance3 == null) {
+            instance3 = new BranchCover();
+        }
+        return instance3;
+    }
+
+    public static BranchCover getInstance4() {        
+        if (instance4 == null) {
+            instance4 = new BranchCover();
+        }
+        return instance4;
+    }
+
+    public static BranchCover getInstance5() {        
+        if (instance5 == null) {
+            instance5 = new BranchCover();
+        }
+        return instance5;
     }
 
     private BranchCover() {
